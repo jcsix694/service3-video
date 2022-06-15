254398c97eb26545def0b06d936e46ef6d461531
Jake Sixsmith <jcsix694@gmail.com>
2022-06-15 11:49:58 +0100

    REMOVED first push

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/jcsix694/service3-video
 
 go 1.18
-
-require github.com/ardanlabs/conf v1.5.0
-
-require github.com/dimfeld/httptreemux/v5 v5.4.0
--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"github.com/ardanlabs/conf"
-	"github.com/dimfeld/httptreemux/v5"
-)
-
 func main() {
-	conf.New()
-	httptreemux.New()
 }
