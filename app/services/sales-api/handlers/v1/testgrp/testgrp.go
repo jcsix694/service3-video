8fadc596a7275300a5013a1ccd9934e480a5fb19
Jake Sixsmith <jcsix694@gmail.com>
2022-06-20 10:14:10 +0100

    ADDED response foundation

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,7 +2,7 @@ package testgrp
 
 import (
 	"context"
-	"encoding/json"
+	"github.com/jcsix694/service3-video/foundation/web"
 	"go.uber.org/zap"
 	"net/http"
 )
@@ -24,5 +24,5 @@ func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Reque
 
 	h.Log.Infow("readiness", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }
