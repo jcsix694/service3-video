f1163ae0941cd347a201f086b1d509fa106ab30b
Jake Sixsmith <jcsix694@gmail.com>
2022-06-22 15:14:00 +0100

    ADDED seed data

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -25,6 +25,25 @@ func main() {
 	}
 }
 
+func seed(cfg database.Config) error {
+	db, err := database.Open(cfg)
+
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
+}
+
 func migrate() error {
 	cfg := database.Config{
 		User:         "postgres",
@@ -49,7 +68,7 @@ func migrate() error {
 	}
 
 	fmt.Println("migrations complete")
-	return nil
+	return seed(cfg)
 }
 
 func genToken() error {
