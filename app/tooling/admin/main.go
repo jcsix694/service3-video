29c3e5379a4476375596dc26a1a6635175e69b15
Jake Sixsmith <jcsix694@gmail.com>
2022-06-22 15:06:25 +0100

    ADDED database migrations

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"github.com/jcsix694/service3-video/business/data/schema"
+	"github.com/jcsix694/service3-video/business/sys/database"
 	"io"
 	"os"
 	"time"
@@ -15,11 +18,38 @@ import (
 )
 
 func main() {
-	err := genToken()
+	err := migrate()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func migrate() error {
+	cfg := database.Config{
+		User:         "postgres",
+		Password:     "postgres",
+		Host:         "localhost",
+		Name:         "postgres",
+		MaxIdleConns: 0,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+	return nil
 }
 
 func genToken() error {
